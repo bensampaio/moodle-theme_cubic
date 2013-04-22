3644d59820db78cd1b6df424fc6a890b8ca1e855
Bruno Sampaio <bens.sampaio@gmail.com>
2013-04-22 20:19:24 +0100

    v1.0.4.1
    Spanish added.

---
+++ lang/es/theme_cubic.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Strings for component 'theme_cubic', language 'es', branch 'MOODLE_22_STABLE'
+ * @copyright 2013 Bruno Sampaio, Portugal
+ */
+
+/* Theme
+------------------------*/
+$string['pluginname'] = 'Cubic';
+$string['region-side-post'] = 'Derecha';
+$string['region-side-pre'] = 'Izquierda';
+
+$string['choosereadme'] = 
+	'<div class="clearfix">
+		<div class="theme_screenshot">
+			<h2>Cubic</h2>
+			<img src="cubic/pix/screenshot.png" />
+		</div>
+		<div class="theme_description">
+			<h2>Acerca de</h2>
+			<p>Cubic es ampliamente fluido, Es un tema de tres columnas para Moodle 2.3. Su objetivo es proveer a los usuarios de una interfaz más amigable, que contiene múltiples formas de conciencia social y algunos nuevos conceptos.</p>
+			<h2>Ajustes</h2>
+			<p>Este tema es un tema de base para Moodle, el cual introduce algunos nuevos conceptos y que toma ventaja de algunas nuevas tecnologias como HTML5 y CSS3.</p>
+			<p>En el principio de toda página de Moodle, este tema introduce una barra de usuario que provee a los usuarios de diferentes menus. Cada uno de estos menus contiene información acerca de los cursos, actividades, notificaciones, eventos, mensajes, configuraciones, y otros en los que el usuario está activo o matriculado.</p>
+			<p>Además de eso, este tema esta relacionado con dos nuevos plugins locales de Moodle, que introduce los conceptos <a href="https://moodle.org/plugins/view.php?plugin=local_institutions">Instituciones</a> y el <a href="https://moodle.org/plugins/view.php?plugin=local_applications">Aplicaciones</a> en Moodle. En la barra de menu de usuario se muestra una sección para cada uno de estos conceptos. Dichos menus permiten a los usuarios ingresar a las páginas de las instituciones o las aplicaicones, respectivamente. Tanto las páginas de Instituciones como de Aplicaciones pueden ser consultadas sin abandonar Moodle.</p>
+			<p>Además, este tema también esta relacionado con un nuevo bloque de Moodle <a href="https://moodle.org/plugins/view.php?plugin=block_gchat">Bloque Plugin</a> que introduce un servicio de chat para Moodle que esta disponible para todos, un poco como los chats incorporados de Google o Facebook.</p>
+			<p>Desde que la barra de usuarios contiene mucha información útil, los bloques ya no son necesarios en algunos contextos. Por ejemplo, Cuando se embebe una página, Cubic utiliza tanto el espacio vertical como el horizontal dejando únicamente la barra de usuarios en la parte superior.</p>
+			<p>Finalmente, este tema también provee unas configuraciones de página donde puedes cambiar muchas de las propiedades como lo son logos, colores, nombre de sistema, entre otros. En esta página también puedes remover los menus de las instituciones y las aplicaciones de la barra de usuarios en caso de que no quieras usarlas.</p>
+			<h2>Creditos</h2>
+			<p>Este tema fue codificado y es mantenido por <a href="http://pessoa.fct.unl.pt/b.sampaio/index.php?language=en">Bruno Sampaio</a>. La decisión de crear este tema fue tomada durante su tésis de Master, que estaba relacionada con el uso de las nuevas tecnologías en educación, especialmente video juegos. Moodle fue elegido como una forma de entregar o distribuir el desarrollo de juegos educativos <a href="http://epik.di.fct.unl.pt/epik">Epik</a>, y al apuntar muchos estudios problemas con la interfaz de usuario, se decidió crear un nuevo tema para satisfacer algunos de los conceptos de Entornos Personales de Aprendizaje (PLEs Personal Learning Enviroment por sus siglas en ingles o EPAs por sus siglas en español).</p>
+			<p>Este tema no sería tan maravilloso sin la ayuda de <b>André Costa</b> quien diseño el logo de Cubic y <b>Sónia Martins</b> quien diseño algunos de los iconos usados. Además de eso algunos de los iconos y scripts usados estan disponibles en internet.</p>
+			<p>Puedes encontrar los iconos de la barra de usarios <a href="http://www.iconfinder.com/search/15/?q=iconset%3Asuper-mono-reflection">aqui</a> (creado por <a href="http://www.doublejdesign.co.uk/">Double-J Design</a>) y los íconos monocromáticos <a href="http://www.iconfinder.com/search/?q=iconset%3Acc_mono_icon_set">aqui</a> (created by <a href="http://www.gentleface.com/">GentleFace</a>), ambos bajo licencia<a href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons</a>.</p>
+			<p>Los Javascript scripts usados donde el jQuery framework se encuentran <a href="http://jquery.com/">aqui</a>, el jQuery UI puedes encontrarlo <a href="http://jqueryui.com/">aqui</a> y el jQuery Custom Scrollbar puedes encontrarlo <a href="http://manos.malihu.gr/jquery-custom-content-scroller">aqui</a>.</p>
+			<p>Si necesitas ayuda con cualquier cosa relacionada con este tema sólo envía un e-mail a b.sampaio@campus.fct.unl.pt y trataré de responder tan rápido como me sea posible.</p>
+			<h2>Extras</h2>
+			<p>Además de este tema nosotros hemos creado dos bloques. Uno para mostrar las Aplicaciones favoritas del usuario. Y otra con un Chat Global, similar a las usadas en Google y Facebook, por ejemplo.</p>
+		</div>
+	</div>';
+
+/* Menus
+------------------------*/
+$string['guest'] = 'Invitado';
+$string['nouser-info'] = 'Acceso Limitado.';
+
+$string['menu-intitutions'] = 'Instituciones';
+$string['menu-courses'] = 'Cursos';
+$string['menu-applications'] = 'Aplicaciones';
+$string['menu-activities'] = 'Actividades';
+
+$string['menu-notifications'] = 'Notificaciones';
+$string['menu-events'] = 'Eventos';
+$string['menu-messages'] = 'Mensajes';
+$string['menu-languages'] = 'Lenguages';
+$string['menu-info'] = 'Tú';
+$string['menu-settings'] = 'Configuraciones';
+
+$string['settings-edit'] = 'Editar Perfil';
+$string['settings-password'] = 'Cambiar Contraseña';
+$string['settings-msg'] = 'Mensajería';
+$string['settings-logout'] = 'Salir';
+$string['settings-login'] = 'Entrar';
+$string['settings-reset'] = 'Restaurar Contraseña';
+
+$string['empty-institutions'] = 'No hay Instituciones';
+$string['empty-courses'] = 'No esta matriculado en ningun Curso';
+$string['empty-applications'] = 'No tiene Aplicaciones';
+$string['empty-activities'] = 'No tiene Actividades';
+$string['empty-notifications'] = 'No tiene Notificaciones';
+$string['empty-events'] = 'No tiene nuevos Eventos';
+$string['empty-messages'] = 'No tiene nuevos Mensajes';
+
+$string['see-all'] = 'Ver todos los';
+
+$string['plugin-missing'] = 'Para usar este componente necesitas instalar otro plug in que puedes encontrar aqui:';
+$string['plugin-note'] = 'También puedes remover este componente de la barra de menu de usuario en la página de configuraciones del tema.';
+
+/* Settings
+------------------------*/
+$string['configtitle'] = 'Cubic';
+
+$string['title'] = 'Título';
+$string['title_desc'] = 'Nombre que aparecerá en el footer.';
+
+$string['show_institutions'] = 'Mostrar Instituciones';
+$string['show_institutions_desc'] = 'Mostrar la sección Instituciones en la barra de usuario.';
+
+$string['show_applications'] = 'Mostrar Aplicaciones';
+$string['show_applications_desc'] = 'Mostrar la sección Aplicaciones en la barra de usuario.';
+
+$string['logo'] = 'Logo';
+$string['logo_desc'] = 'URL para la imágen de logo principal.';
+
+$string['bar_logo'] = 'Logo de la barra';
+$string['bar_logo_desc'] = 'URL para la imagen del logo en el top de la barra. Este URL debe referenciar un archivo dentro de la carpeta pix de este tema sin su extensión (Por ejemplo: Si tienes el archivo "/theme/cubic/pix/new_logo.png", el valor para este campo debe ser "new_logo").';
+
+$string['feedback_desc'] = 'URL para página de retroalimentación (al proporcionar este URL un botón de retroalimentación se mostrará del lado derecho de todas las páginas del Moodle).';
+
+$string['bar_color'] = 'Color de la barra';
+$string['bar_color_desc'] = 'Color de fondo para la barra superior.';
+
+$string['bar_border_color'] = 'Color del borde de la barra';
+$string['bar_border_color_desc'] = 'Color superior del borde para la barra superior.';
+
+$string['bar_text_color'] = 'Color del Texto de la barra';
+$string['bar_text_color_desc'] = 'Color de texto para la barra superior.';
+
+$string['bar_hover_color'] = 'Color Hover de la barra';
+$string['bar_hover_color_desc'] = 'Color Hover para los botones de la barra superior.';
+
+$string['menu_all_color'] = 'Color de fondo de la sección inferior del menu';
+$string['menu_all_color_desc'] = 'Color de fondo para la sección inferior en algunos menus.';
+
+$string['text_color'] = 'Color de Texto';
+$string['text_color_desc'] = 'Color para todos los encabezados y párrafos.';
+
+$string['hover_color'] = 'Color Hover';
+$string['hover_color_desc'] = 'Color hover principal para los elementos de página.';
+
+$string['links_color'] = 'Color de Links';
+$string['links_color_desc'] = 'Color de todos los links.';
+
+$string['borders_color'] = 'Color de Bordes';
+$string['borders_color_desc'] = 'Color de los bordes principales.';
+
+$string['button_top_color'] = 'Color de los botones superiores';
+$string['button_top_color_desc'] = 'Color principal de todos los botones.';
+
+$string['button_bottom_color'] = 'Color inferior de los botones';
+$string['button_bottom_color_desc'] = 'Color inferior de todos los botones.';
+
+$string['button_border_color'] = 'Color del borde de botones';
+$string['button_border_color_desc'] = 'Color del borde de todos los botones.';
+
+$string['activity_view'] = 'Visualización de Actividades';
+$string['activity_view_desc'] = 'Mode de visualización de actividades predeterminada en páginas del curso.';
+$string['activity_icons_view'] = 'Iconos';
+$string['activity_list_view'] = 'Lista';
+
+$string['custom_css'] = 'Configurar CSS';
+$string['custom_css_desc'] = 'Mostrar reglas de configuración CSS aqui.';
